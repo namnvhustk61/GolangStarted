0a2ce321827ebe06098daa21c24d0f5f2a1e20d7
namnvhustk61 <79648470+namnvhustk61@users.noreply.github.com>
2023-12-26 00:32:52 +0700

    Update create_item_handler.go
    .

--- social-todo-list/modules/item/transport/gin/create_item_handler.go
+++ social-todo-list/modules/item/transport/gin/create_item_handler.go
@@ -1,9 +1,11 @@
-package gin
+package ginitem
 
 import (
 	"net/http"
 	"social-todo-list/common"
+	"social-todo-list/modules/item/biz"
 	"social-todo-list/modules/item/model"
+	"social-todo-list/modules/item/storage"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -14,17 +16,20 @@ func CreateItem(db *gorm.DB) func(*gin.Context) {
 		var data model.TodoaitemCreation
 		if err := ctx.ShouldBind(&data); err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{
-				"message": err.Error(),
+				"error": err.Error(),
 			})
 			return
 		}
 
-		if err := db.Create(&data).Error; err != nil {
+		store := storage.NewSQLStore(db)
+		business := biz.CreateItemStorage(store)
+		if err := business.CreateItem(ctx.Request.Context(), &data); err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{
-				"message": err.Error(),
+				"error": err.Error(),
 			})
 			return
 		}
+
 		ctx.JSON(http.StatusOK, common.SimpleSuccessResponse(data.Id))
 	}
 }
